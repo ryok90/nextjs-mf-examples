043e8f085f5d5451e28eb7910c649ac2e2b62fb3
Rodrigo Yokota <ryok90@gmail.com>
2023-12-06 16:41:52 -0300

    feat: base example

---
+++ next13.3.1-mf6.4.0/apps/remote/next.config.js
@@ -0,0 +1,27 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config, options) {
+    const { isServer } = options;
+    const remoteDir = isServer ? 'ssr' : 'chunks';
+
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'remote',
+        filename: `static/${remoteDir}/remoteEntry.js`,
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+        exposes: {
+          './table': './components/table/table',
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
