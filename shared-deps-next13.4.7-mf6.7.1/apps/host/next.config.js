94bc6a003cf17d0155afd928cb87f44ea3434509
Rodrigo Yokota <ryok90@gmail.com>
2023-12-07 09:58:07 -0300

    feat: add two 13.4 working examples

---
+++ shared-deps-next13.4.7-mf6.7.1/apps/host/next.config.js
@@ -0,0 +1,36 @@
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+const lo = require('../../../local-package/v1.0.0');
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config, options) {
+    const { isServer } = options;
+    const remoteDir = isServer ? 'ssr' : 'chunks';
+
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'host',
+        filename: `static/${remoteDir}/remoteEntry.js`,
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+        remotes: {
+          remote: `remote@http://localhost:3011/_next/static/${remoteDir}/remoteEntry.js`,
+        },
+        shared: {
+          'local-package': {
+            version: '1.0.0',
+            requiredVersion: '1.0.0',
+            strictVersion: true,
+            // singleton: true,
+          },
+        },
+      })
+    );
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
